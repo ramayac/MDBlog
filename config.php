15efac96107ed9af53c373cc2033c37ce794a350
Rodrigo Amaya <ramayac@gmail.com>
2025-09-27 11:07:02 -0700

    clean up

--- config.php
+++ config.php
@@ -9,21 +9,15 @@
 
 // Basic blog settings
 return [
-    'blog_name' => 'Rodrigo A. ',
+    'blog_name' => 'MD Blog ',
     // Header and footer content
-    'header_content' => 'Wholesome Software Development.',
+    'header_content' => 'A simple PHP + Markdown blog.',
     // Category configuration
     'categories' => [
-        'srbyte' => [
-            'blog_name' => 'Sr. Byte 👨‍💻',
-            'header_content' => '(2007-2010) Tecnologia para todos.',
-            'folder' => 'srbyte',
-            'index' => false, //shows it in the main index
-        ],
-        'substack' => [
-            'blog_name' => 'Code Forward ⏩',
-            'header_content' => '(2023) Substack failed newsletter.',
-            'folder' => 'substack',
+        'old' => [
+            'blog_name' => 'Old musings 🎵',
+            'header_content' => 'Old thoughts before 2020.',
+            'folder' => 'old',
             'index' => false, //shows it in the main index
         ],
     ],
--- posts/srbyte/srbyte-quien-dijo-baile.md
+++
@@ -1,9 +0,0 @@
----
-title: ¿Quien dijo baile?
-date: 2007-01-24
-author: Rodrigo Amaya
-tags: baile
-post_id: blog-3515952828243908885.post-2530025935677560882
----
-
-Decidí colocar unos vídeos muy interesantes y divertidos para resumir las actividades por aquí:
