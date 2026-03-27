143271fe72c9079966834c7d63f9ee61134db663
ramayac <506032+ramayac@users.noreply.github.com>
2026-03-27 12:26:06 -0700

    small improvements

--- config.php
+++ config.php
@@ -87,5 +87,6 @@ return [
     'csp_header' => "Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:;",
 
     // CSS theme
-    'css_theme' => 'assets/css/default.style.css',
+    // 'css_theme' => 'assets/css/default.style.css',
+    'css_theme' => 'assets/css/scrum.style.css',
 ];
